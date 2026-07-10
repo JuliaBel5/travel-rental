3c953024b17dd28b6800d4bf64a4daa19bae689c
Julia Chekan <juliabel5@gmail.com>
2026-07-11 01:09:01 +0300

    feat: add email/password auth foundation (NextAuth credentials, User model, register)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,23 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
 import { Layout } from "@/components/layout/Layout";
 
 const geistSans = Geist({ variable: "--font-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <div className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <div className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
